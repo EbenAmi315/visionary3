e3addd39467cd039a6a0cadcf469a26e121cc875
Glitch (splashy-laced-harpymimus) <none>
2022-04-29 22:06:28 +0000

    🍫💼 Checkpoint ./events/ready.js:43843471/314

--- events/ready.js
+++ events/ready.js
@@ -2,15 +2,17 @@ const client = require("../index");
 client.on("ready", () => {
     console.log(`${client.user.tag} İsmi İle Bot Aktif!`)
 const sunucu = client.guilds.cache.size.toLocaleString()
+const kullanıcı = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()
 
  var oyun = [
        "Visionary 2022 Büyüyoruz!",
        "Yeni Ortak! ✬ mexelynd#1881",
-       `Visionary Şu an ${sunucu} sunucuda`];
+       `Visionary Şu an ${sunucu} sunucuda`,
+       `Visionary ${kullanıcı} Kullanıcıya Hizmet Veriyor`];
 
   setInterval(function() {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
     client.user.setActivity(oyun[random], "");
-  }, 3 * 2500);
+  }, 4 * 2500);
 });
