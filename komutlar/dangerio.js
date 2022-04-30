01b96d9e1cecf508764aef26235d702df43a4710
Glitch (splashy-laced-harpymimus) <none>
2022-04-30 20:24:55 +0000

    🍶🎰 Checkpoint ./komutlar/dangerio.js:43843471/8435 ./komutlar/ping.js:43843471/5600

--- komutlar/dangerio.js
+++ komutlar/dangerio.js
@@ -2,37 +2,17 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
         const start = Date.now();
-        message.channel.send('𝗗𝗮𝗻𝗴𝗲𝗿𝗶𝗼 𝗛𝗼𝘀𝘁𝗶𝗻𝗴
-Hostingmi arıyorsun İşte tam yerine geldin Güçlü sunucularıyla Dangerio Hosting
-
-𝗛𝗼𝘀𝘁𝗶𝗻𝗴𝗹𝗲𝗿𝗶𝗺𝗶𝘇
-Visual Studio Code
-Minecraft Server
-Minecraft Bot
-Site
-Wordpress
-Obs 
-Google Chrome
-Windows Vds
-Linux ssh Vds
-Cloud
-
-𝗢̈𝘇𝗲𝗹𝗹𝗶𝗸𝗹𝗲𝗿
-7/24 Aktif destek,
-Cep dostu ekonomik fiyatlar
-Yüksek hızda makinalar 
-Yeni başlayanlar için ideal
-Sponsorluk hizmeti
-
-𝐒𝐞𝐧𝐝𝐞 𝐩𝐫𝐨𝐟𝐞𝐬𝐲𝐨𝐧𝐞𝐥 𝐛𝐢𝐫 𝐡𝐢𝐳𝐦𝐞𝐭 𝐢𝐬𝐭𝐢𝐲𝐨𝐫𝐬𝐚𝐧
-Giriş Anahtarın 🔑 discord.gg/4Xpwwz6pgN').then(m => {
-            const embed = new Discord.MessageEmbed()
+        message.channel.send('𝗗𝗮𝗻𝗴𝗲𝗿𝗶𝗼 𝗛𝗼𝘀𝘁𝗶𝗻𝗴 Hostingmi arıyorsun İşte tam yerine geldin Güçlü sunucularıyla Dangerio Hosting').then(m => {
+        message.channel.send ('𝗛𝗼𝘀𝘁𝗶𝗻𝗴𝗹𝗲𝗿𝗶𝗺𝗶𝘇 Visual Studio Code Minecraft Server Minecraft BotSite Wordpress Obs Google Chrome Windows Vds Linux ssh VdsCloud').then(m => {
+        message.channel.send ('𝗢̈𝘇𝗲𝗹𝗹𝗶𝗸𝗹𝗲𝗿 7/24 Aktif destek, Cep dostu ekonomik fiyatlar Yüksek hızda makinalar  Yeni başlayanlar için ideal Sponsorluk hizmeti').then(m => {
+        message.channel.send ('𝐒𝐞𝐧𝐝𝐞 𝐩𝐫𝐨𝐟𝐞𝐬𝐲𝐨𝐧𝐞𝐥 𝐛𝐢𝐫 𝐡𝐢𝐳𝐦𝐞𝐭 𝐢𝐬𝐭𝐢𝐲𝐨𝐫𝐬𝐚𝐧 Giriş Anahtarın 🔑 discord.gg/4Xpwwz6pgN').then(m => {     
+           const embed = new Discord.MessageEmbed()
                 .setColor('BLUE')
-                .setTitle(client.user.username + " - Pong!")
-                .setThumbnail(client.user.displayAvatarURL())
-                .addField(`  `)
-                .setFooter({ text: 'Visionary', iconURL: message.author.avatarURL({ dynamic: true }) });
+                .setTitle(client.user.username + "")
             m.edit({ embeds: [embed] });
+})
+})
+})
 })
 };
 exports.conf = {
